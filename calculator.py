e42331217a3bf11351ba6bbb3453d35a9939e2f3
bilqueeshshahb <bilqueeshshahb@gmail.com>
2026-01-21 15:52:57 +0530

    Add files via upload

---
+++ calculator.py
@@ -0,0 +1,20 @@
+2. Write a program to input 2 numbers and one arithmetic operator. Perform operations as per arithmetic operator which is given as input
+
+a = int(input("enter 1st number: "))
+b = int(input("enter 2nd number: "))
+
+addition = a + b
+subtraction = a - b
+multiplication = a * b
+division = a / b
+
+#print(f"addition of {a} and {b} is : {addition}")
+#print(f"subtraction of {a} and {b} is : {subtraction}")
+#print(f"multiplication of {a} and {b} is : {multiplication}")
+#print(f"division of {a} and {b} is : {division}")
+#-------------------------------------------------or--------------------------
+
+print("addition of", a, "and", b, "is :", addition)
+print("subtraction of", a, "and", b, "is :", subtraction)
+print("multiplication of", a, "and", b, "is :", multiplication)
+print("division of", a, "and", b, "is :", division)
