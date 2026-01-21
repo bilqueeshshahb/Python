6491bf4a79b8e7ddb59ec1e3405cad8536ade89f
bilqueeshshahb <bilqueeshshahb@gmail.com>
2026-01-21 15:57:46 +0530

    Add files via upload

---
+++ histogram.py
@@ -0,0 +1,10 @@
+#6.define a procedure histogram()that takes a list of intergers & prints a
+#histogram to screen.for e.g.([4,9,7]) should print.
+
+def histogram(numbers):
+    for n in numbers:
+        print('*' * n )
+        
+histogram([4,9,7])        
+        
+
